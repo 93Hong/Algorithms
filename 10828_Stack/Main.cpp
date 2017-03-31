604b87b4d0c5f94546e2c39e1bfd92d82306afec
93Hong <sk2rldnr@gmail.com>
2017-03-31 23:26:12 +0900

    stack 2 problem done

--- 10828_Stack/Main.cpp
+++ 10828_Stack/Main.cpp
@@ -4,26 +4,53 @@
 
 using namespace std;
 
-void push(int x) {
+int stack[10000], curser;
 
+void push(int x) {
+	stack[++curser] = x;
 }
 
 int pop() {
-	return;
+	if (curser == -1)
+		return -1;
+	return stack[curser--];
 }
 
 int size() {
-	return;
+	return curser+1;
 }
 
 int empty() {
-	return;
+	if (curser == -1)
+		return 1;
+	return 0;
 }
 
 int top() {
-	return;
+	if (curser == -1)
+		return -1;
+	return stack[curser];
 }
 
 int main() {
+	curser = -1;
+	int N, num;
+	char str[6];
+	scanf("%d", &N);
 
+	for (int i = 0; i < N; i++) {
+		scanf("%s", &str);
+		if (str[1] == 'u') {
+			scanf("%d", &num);
+			push(num);
+		}
+		else if (str[0] == 't')
+			printf("%d\n", top());
+		else if (str[0] == 's')
+			printf("%d\n", size());
+		else if (str[0] == 'e')
+			printf("%d\n", empty());
+		else
+			printf("%d\n", pop());
+	}
 }
---
+++ 1874_SeriesStack/Main.cpp
@@ -0,0 +1,45 @@
+#include <cstring>
+#include <cstdio>
+#include<stack>
+#pragma warning(disable: 4996)
+
+using namespace std;
+stack<int> s;
+char arr[200001];
+
+int main() {
+	int N;
+	int num, top = 0, cIndex = 0;
+	bool flag = false;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &num);
+		if (num > top) {
+			for (int j = top + 1; j <= num; j++) {
+				s.push(j);
+				arr[cIndex++] = '+';
+			}
+			top = num;
+			arr[cIndex++] = '-';
+			if(!s.empty()) s.pop();
+		}
+		else {
+			if (num != s.top()) {
+				flag = true;
+				break;
+			}
+			if(!s.empty())s.pop();
+			arr[cIndex++] = '-';
+		}
+		//top = s.top();
+	}
+
+	if (flag) {
+		printf("NO\n");
+	}
+	else {
+		for (int i = 0; i < cIndex; i++)
+			printf("%c\n", arr[i]);
+	}
+}
