77cf3e5f6e017b997d5d4885b9c8aa8d6c27320e
93Hong <sk2rldnr@gmail.com>
2017-04-01 23:00:30 +0900

    stack 1 pro done

--- 12789_Okydoky/Main.cpp
+++ 12789_Okydoky/Main.cpp
@@ -7,36 +7,36 @@ stack<int> st;
 
 int main() {
 	int N, num, cnt = 1;
-	int line[1001];
+	int line[3000], line2[3000];
 	scanf("%d", &N);
 
 	for (int i = 0; i < N; i++) {
 		scanf("%d", &num);
 		line[i] = num;
+		line2[i] = 1001;
 	}
-	int i;
-	for (i = 0; i < N; i++) {
-		if (line[i] != cnt) {
-			if (st.empty())
-				st.push(line[i]);
-			else if (st.top() < line[i])
-				break;
-		}
-		else
-			cnt++;
-	}
-
+	int i, j = 1;
 	bool flag = true;
 
-	for (i; i < N - 1; i++) {
-		if (line[i] > line[i + 1]) {
-			flag = false;
-			break;
+	for (i = 0; i < N; i++) {
+		if (line[i] == cnt) {
+			cnt++;
+		}
+		else if (line2[j-1] == cnt) {
+			cnt++;
+			j--;
+			i--;
+		}
+		else {
+			line2[j] = line[i];
+			if (line2[j] > line2[j - 1]) {
+				printf("Sad\n");
+				flag = false;
+				break;
+			}
+			j++;
 		}
 	}
-
 	if (flag)
 		printf("Nice\n");
-	else
-		printf("Sad\n");
 }
