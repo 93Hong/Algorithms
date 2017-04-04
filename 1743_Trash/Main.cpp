635eb34d5c168ae8b65e52f1b8697e02918d25e2
93Hong <sk2rldnr@gmail.com>
2017-04-04 17:28:35 +0900

    today last pro done

--- 1743_Trash/Main.cpp
+++ 1743_Trash/Main.cpp
@@ -1,67 +1,47 @@
-
 #include <cstdio>
-#include <cstring>
+#include <queue>
 #pragma warning(disable: 4996)
 
 using namespace std;
+int map[101][101];
+bool check[101][101];
+queue<pair<int, int>> q;
 
 int main() {
-	int M=0, N, K, x, y;
-	scanf("%d", &M); scanf("%d", &N); scanf("%d", &K);
+	int M = 0, N, K, x, y;
+	scanf("%d", &N); scanf("%d", &M); scanf("%d", &K);
 
-	int map[101][101], tmp[100], re[100], max = 0;
-	memset(map, 0, sizeof(map));
-	memset(tmp, 0, sizeof(tmp));
-	memset(re, 0, sizeof(re));
+	int dir[4][2] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
 
 	for (int i = 0; i < K; i++) {
 		scanf("%d", &x); scanf("%d", &y);
-		map[x-1][y-1] = 1;
+		map[x - 1][y - 1] = 1;
 	}
 
-	int cur = 0, cnt = 0, val=0;
-	bool flag = true;
-	
-	for (int a = 0; a < M; a++) {
-		for (int i = 0; i < N + 1; i++) {
-			if ((map[a][i] == 0 && !flag)) {
-				if (max < cnt)
-					max = cnt;
-				for (int j = 0; j < cnt; j++) {
-					map[a][j + cur] = val;
-				}
-				flag = true;
-				cur = 0;
-				cnt = 0;
-				val = 0;
-			}
-			else if (map[a][i] != 0) {
-				cnt++;
-				val += map[a][i];
-				if (flag) {
-					cur = i;
-					flag = false;
+	int cnt = 0, max = 0;
+	int i, j;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (map[i][j] == 1 && !check[i][j]) {
+				q.push({ i, j });
+				check[i][j] = true;
+				while (!q.empty()) {
+					for (int k = 0; k < 4; k++) {
+						int x = q.front().first + dir[k][0], y = q.front().second + dir[k][1];
+						if (map[x][y] == 1 && !check[x][y]) {
+							q.push({ x, y });
+							check[x][y] = true;
+						}
+					}
+					q.pop();
+					cnt++;
 				}
 			}
+			if (max < cnt)
+				max = cnt;
+			cnt = 0;
 		}
-
-		for (int j = 0; j < N; j++) {
-			printf("%d", map[a][j]);
-		}
-		printf("\n");
-
-		for (int j = 0; j < N; j++) {
-			if (map[a+1][j]) {
-				map[a+1][j] += map[a][j];
-				if (max < map[a+1][j])
-					max = map[a+1][j];
-			}
-		}
-		for (int j = 0; j < N; j++) {
-			printf("%d", map[a+1][j]);
-		}
-		printf("\n");
 	}
-
 	printf("%d\n", max);
 }
