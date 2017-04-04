e3d00ff558a6fd44dfabc8ce610538909af1f0ce
93Hong <sk2rldnr@gmail.com>
2017-04-04 13:02:42 +0900

    fibo all done

--- 2747_Fibo/Main.cpp
+++ 2747_Fibo/Main.cpp
@@ -3,19 +3,26 @@
 
 using namespace std;
 
-long long fibo[100];
+long long fibo[41];
+long long fibo2[41];
 
 int main() {
 	int n;
-	scanf("%d", &n);
-
+	n = 40;
 	fibo[0] = 0;
 	fibo[1] = 1;
+	fibo2[0] = 1;
+	fibo2[1] = 0;
 
 	for (int i = 2; i < n + 1; i++) {
 		fibo[i] = fibo[i - 1] + fibo[i - 2];
-		fibo[i] %= 1000000;
+		fibo2[i] = fibo2[i - 1] + fibo2[i - 2];
 	}
 
-	printf("%lld\n", fibo[n]);
+	int T, num;
+	scanf("%d", &T);
+	while (T--) {
+		scanf("%d", &num);
+		printf("%lld %lld\n", fibo2[num], fibo[num]);
+	}
 }
