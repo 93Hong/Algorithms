20e8fa21ed9633d77221ad43211b0bdf71c971b7
93Hong <sk2rldnr@gmail.com>
2017-06-15 16:16:36 +0900

    nexonGT 3

--- NexonGT_3/Main.cpp
+++ NexonGT_3/Main.cpp
@@ -4,25 +4,12 @@
 
 using namespace std;
 
-
-bool isInteger(float k) {
-	return floor(k) == k;
-}
-
 int main() {
 	long N;
 	scanf("%ld", &N);
-
 	int ans = 0;
-	iswdigit(3);
-	for (int i = 2; i < 1000001; i++) {
-		int a = (i*i - i) / 2;
-		double tmp = (N - a) / 2;
-		if (isInteger(tmp))
-			ans++;
-		if (a > N)
-			break;
-	}
+
+	for (long i = 2, a = 1; i < 2000000 && a < N; i++, a = (i*i - i) / 2) if ((N - a) % i == 0) ans++;
 
 	printf("%d\n", ans);
 }
