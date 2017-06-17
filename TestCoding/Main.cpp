3ffc0b20f532b3f13aba9ff4c4f4aef3954fba09
93Hong <sk2rldnr@gmail.com>
2017-06-17 17:01:07 +0900

    sk c&c pro

--- TestCoding/Main.cpp
+++ TestCoding/Main.cpp
@@ -1,8 +1,27 @@
-#include <cstdio>
-#pragma warning (disable: 4996)
-
+#include <string>
+#include <vector>
 using namespace std;
 
-int main() {
-	printf("Hello World!\n");
+vector<int> solution(vector<int> truck, vector<int> w) {
+	vector<int> answer;
+	bool ch = false;
+
+	for (int i = 0; i < w.size(); i++) {
+		for (int j = 0; j < truck.size(); j++) {
+			if (w[i] <= truck[j]) {
+				truck[j] -= w[i];
+				answer.push_back(j + 1);
+				ch = true;
+				break;
+			}
+		}
+
+		if (ch)
+			ch = false;
+
+		else
+			answer.push_back(-1);
+	}
+
+	return answer;
 }
