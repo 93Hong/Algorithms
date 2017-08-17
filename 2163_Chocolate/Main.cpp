b329ef94640eda84dbd229aad1fb44b389879d34
93Hong <sk2rldnr@gmail.com>
2017-08-18 00:08:48 +0900

    chocolate done

--- 2163_Chocolate/Main.cpp
+++ 2163_Chocolate/Main.cpp
@@ -4,5 +4,14 @@
 using namespace std;
 
 int main() {
-
+	int N, M;
+	scanf("%d", &N); scanf("%d", &M);
+	int ans = 0;
+	if (N > M) {
+		ans = (M - 1) + (M * (N - 1));
+	}
+	else {
+		ans = (N - 1) + (N * (M - 1));
+	}
+	printf("%d\n", ans);
 }
