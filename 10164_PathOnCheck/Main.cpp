d640467ecf12b1f2c5ee700c6ffe33c4763a4106
93Hong <sk2rldnr@gmail.com>
2017-08-28 22:35:10 +0900

    10164_PathOnCheck

---
+++ 10164_PathOnCheck/Main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int map[16][16], dp[16][16];
+
+int main() {
+	int N, M, K;
+	scanf("%d", &N); scanf("%d", &M); scanf("%d", &K);
+
+	if (K == 0) {
+
+	}
+	else {
+
+	}
+
+
+}
