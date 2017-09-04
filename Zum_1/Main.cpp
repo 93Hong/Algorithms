bcec79a5c9f7d8429fdb16d518a0f8faaeae823a
93Hong <sk2rldnr@gmail.com>
2017-09-04 22:10:18 +0900

    code test comment

--- Zum_1/Main.cpp
+++ Zum_1/Main.cpp
@@ -9,6 +9,7 @@ string solution(int A, int B, int C, int D, int E, int F) {
 	vector<int> ve1, ve2;
 	string answer = "";
 
+	// 5를 기준으로 2개의 vector에 저장
 	A > 5 ? ve2.push_back(A) : ve1.push_back(A);
 	B > 5 ? ve2.push_back(B) : ve1.push_back(B);
 	C > 5 ? ve2.push_back(C) : ve1.push_back(C);
@@ -16,20 +17,26 @@ string solution(int A, int B, int C, int D, int E, int F) {
 	E > 5 ? ve2.push_back(E) : ve1.push_back(E);
 	F > 5 ? ve2.push_back(F) : ve1.push_back(F);
 
+	// 가장 빠른 시간을 찾는 것이므로 각각 소팅
 	sort(ve1.begin(), ve1.end());
 	sort(ve2.begin(), ve2.end());
 
+	// 예외처리 (시간으로 표현할 수 없는 경우)
 	if (ve1[0] > 2 || (ve1[0] == 2 && ve1[1] > 3) || ve2.size() >= 4) return "NOT POSSIBLE";
 
+	// ve1로만 출력
 	if (ve2.size() == 0) {
 		answer = to_string(ve1[0]) + to_string(ve1[1]) + ":" + to_string(ve1[2]) + to_string(ve1[3]) + ":" + to_string(ve1[4]) + to_string(ve1[5]);
 	}
+	// ve2를 ss에
 	else if (ve2.size() == 1) {
 		answer = to_string(ve1[0]) + to_string(ve1[1]) + ":" + to_string(ve1[2]) + to_string(ve1[3]) + ":" + to_string(ve1[4]) + to_string(ve2[0]);
 	}
+	// ve2를 mm, ss에
 	else if (ve2.size() == 2) {
 		answer = to_string(ve1[0]) + to_string(ve1[1]) + ":" + to_string(ve1[2]) + to_string(ve1[3]) + ":" + to_string(ve1[4]) + to_string(ve2[0]);
 	}
+	// ve2를 hh, mm, ss에
 	else {
 		answer = to_string(ve1[0]) + to_string(ve2[0]) + ":" + to_string(ve1[1]) + to_string(ve2[1]) + ":" + to_string(ve1[2]) + to_string(ve2[2]);
 	}
