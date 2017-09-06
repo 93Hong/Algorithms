8d68417414123be03acdda53310fc7d62fbd14a2
93Hong <sk2rldnr@gmail.com>
2017-09-06 16:52:10 +0900

    1495 guitaist

---
+++ 1495_Guitarist/Main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int arr[1001], dp[1001];
+
+int main() {
+	int N, S, M;
+	scanf("%d", &N); scanf("%d", &N); scanf("%d", &M);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+
+}
