bdfcb77481298155f180123eac4bfa12ed7a1ebc
93Hong <sk2rldnr@gmail.com>
2017-09-21 20:23:42 +0900

    map sort by value

--- NHN_Enter_4/Main.cpp
+++ NHN_Enter_4/Main.cpp
@@ -8,15 +8,21 @@ using namespace std;
 int main() {
 	map<string, int> m;
 	
-	m.insert(pair<string, int>("aaa", 3));
-	m.insert(pair<string, int>("aab", 1));
-	m.insert(pair<string, int>("aac", 5));
-	m.insert(pair<string, int>("aac", 5));
-	m.insert(pair<string, int>("aac", 5));
-	m.insert(pair<string, int>("bac", 5));
+	m.insert({ "aaa", 3 });
+	m.insert({ "aad", 5 });
+	m.insert({ "aac", 2 });
+	m.insert({ "aab", 1 });
+
+	m.insert({ "aaa", 4 });
+
+
+	printf("%d\n", m.find("aaa")->second);
+	printf("%d\n", m.find("aac")->second);
 
 	vector<pair<string, int>> vec(m.begin(), m.end());
 
 	for (int i = 0; i < vec.size(); i++)
 		printf("%s %d\n", vec[i].first, vec[i].second);
-}	
+
+
+}
