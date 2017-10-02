cd9e5f1630e02f3db8733e35d7d5cc98d790bc76
93Hong <sk2rldnr@gmail.com>
2017-10-02 22:37:09 +0900

    hi

--- NationOf124/Main.cpp
+++ NationOf124/Main.cpp
@@ -1,27 +1,66 @@
 #include<iostream>
 #include<vector>
+#include <deque>
+#include <string>
 
 using namespace std;
 
-char ch[4] = { '0', '1', '2', '4' };
+char ch[4] = { '4', '1', '2' };
 
 string change124(int no)
 {
 	string answer = "";
 
-	while (no != 0) {
+	while (no > 0) {
 		//answer += tostring(ch[no % 3]);
-		printf("%c\n", ch[no % 3]);
+		printf("%c %d\n", ch[no % 3], no);
 		no /= 3;
 	}
 
 	return answer;
 }
 
-int main()
-{
-	int testNo = 10;
-	string testAnswer = change124(testNo);
+int main() {
+	deque<string> q;
+	
+	q.push_back("1");
+	q.push_back("2");
+	q.push_back("4");
+	q.push_back("3");
+	q.push_back("5");
 
-	printf("%s\n", testAnswer);
+	int size = q.size();
+
+	string str = "6";
+
+	if (find(q.begin(), q.end(), str) != q.end()) {
+		for (int i = 0; i < size; i++) {
+			if (q[i].compare(str) == 0) {
+				for (int j = 0; j < size; j++) {
+					if (i == j) {
+						q.pop_front();
+						continue;
+					}
+					string a = q.front();
+					q.pop_front();
+					q.push_back(a);
+				}
+				break;
+			}
+		}
+		q.push_back(str);
+	}
+	else {
+		q.pop_front();
+		q.push_back(str);
+	}
+	for (int i = 0; i < 5; i++) {
+		string c = q.front();
+		printf("%s ", c.c_str());
+		q.pop_front();
+		q.push_back(c);
+	}
+	printf("\n");
+	
+
 }
