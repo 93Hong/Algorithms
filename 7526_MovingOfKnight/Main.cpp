3c6127a21901e75dac338a9e9ee816ed802e8b3f
93Hong <sk2rldnr@gmail.com>
2017-10-23 01:56:42 +0900

    problems

---
+++ 7526_MovingOfKnight/Main.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#pragma warning(disable:4996)
+
+using namespace std;
+int map[301][301];
+int night[8][2] = { { 1, -2 },{ 1, 2 },{ 2, -1 },{ 2, 1 },{ -1, 2 },{ -1, -2 },{ -2, 1 },{ -2, -1 } };
+
+int main() {
+	int n, m;
+
+	scanf("%d", &n);
+
+	while (n--) {
+		scanf("%d", &m);
+
+		
+	}
+}
